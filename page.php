755dbf4b06db10fd18b9ba6c5eb5c4a3be915d27
testwind <31543711+testwind@users.noreply.github.com>
2018-10-12 15:31:11 +0800

    加 page

---
+++ page.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>无标题文档</title>
+<style type="text/css">
+table {
+	border: 1;
+	background-color: #B3EFCC;
+}
+</style>
+</head>
+
+<body>
+<table width="269" border="1">
+  <tbody>
+    <tr>
+      <td width="75">开始时间</td>
+      <td width="78">结束时间</td>
+      <td width="94">长度</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+  </tbody>
+</table>
+</body>
+</html>
